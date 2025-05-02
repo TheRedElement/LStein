e58443ee6ec73aca071608cc5af18920d0f6dd46
TheRedElement <82433625+TheRedElement@users.noreply.github.com>
2025-05-02 15:11:30 +1000

    Initial commit

---
+++ code/_projectbuildingblocks.py
@@ -0,0 +1,90 @@
+
+"""
+    - building blocks that are used in multiple files of the project
+    - use by simply importing
+
+    ```python
+    import importlib
+    
+    import _projectbuildingblocks as pbb
+    importlib.reload(pbb)
+    ```    
+"""
+
+#%%imports
+
+#%%definitions
+class ClassTemplate:
+    """
+        - class description
+
+        Attributes
+        ----------
+            - `verbose`
+                - `int`, optional
+                - verbosity level
+                - the default is `0`
+        
+        Infered Attributes
+        ------------------
+            - 
+
+        Methods
+        -------
+            - 
+
+        Dependencies
+        ------------
+            -
+
+        Comments
+        --------
+    """
+    def __init__(self,
+        verbose:int=0
+        ) -> None:
+        return
+    
+    def __repr__(self) -> str:
+        return (
+            f'{self.__class__.__name__}(\n'
+            f'    verbose={repr(self.verbose)},\n'
+            f')'            
+        )
+    
+    def __dict__(self) -> dict:
+        return eval(str(self).replace(self.__class__.__name__, 'dict'))
+    
+    def __call__(self, *args, **kwds):
+        pass
+    
+def function_template():
+    """
+        - function description
+
+        Parameters
+        ----------
+            - `param_name`
+                - `type`, <optional>
+                - description
+                - the default is `<default>`
+
+
+        Raises
+        ------
+            - `error_type`
+                - 
+
+        Returns
+        -------
+            - `varable_name`
+                - `type`, <optional>
+                - description
+                
+        Dependencies
+        ------------
+
+        Comments
+        --------
+    """
+    return
