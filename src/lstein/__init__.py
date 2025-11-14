02fb5c12bb105244de82a51727a67f2590dbf0b2
Lukas Steinwender <lukas.steinwender99@gmail.com>
2025-11-14 13:16:45 +1100

    version bump to 1.0.0

--- src/lstein/__init__.py
+++ src/lstein/__init__.py
@@ -3,7 +3,7 @@ LStein package
 """
 
 __modulename__ = "LStein" 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 __author__ = "Lukas Steinwender"
 __author_email__ = ""
 __maintainer__ = "Lukas Steinwender"
