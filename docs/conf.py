1c72cf6bc4a621b75a89df62f206ce671911e566
Lukas Steinwender <lukas.steinwender99@gmail.com>
2026-01-08 12:02:20 +1100

    testing readthedocs with uv

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,50 @@
+#configuration for sphinx document builder
+# details: https://www.sphinx-doc.org/en/master/usage/configuration.html
+
 
 #%%imports
 import importlib
+import os
+import sys
+
 
 #%%definitions
+def add_paths():
+    """
+        - function to add directories to be documented to the path
+        - make sure to make the path absolute!
+    """
+    sys.path.insert(0, os.path.abspath("../src"))   #default for uv `project/src/pkg` layout
 
+    return
+
+def set_metadata():
+    #expose to global scope
+    global author
+    global copyright
+    global description
+    global project
+    global title
+    global version
+    global release    
+
+    #read metadata from pyproject.toml
+    metadata = importlib.metadata.metadata("LStein")
+
+    #set metadata
+    author = metadata["Author"]
+    copyright = "2026" + "Lukas Steinwender"
+    description = metadata["Description"]
+    project = metadata["Name"]
+    title = metadata["Name"]
+    version = metadata["Version"]
+    release = metadata["Version"] + ""  #full release (includes alpha, beta, ...)
+
+    return
 
 #%%main
-metadata = importlib.metadata.metadata("LStein")
-author = metadata["Author"]
-copyright = "2026" + "Lukas Steinwender"
-description = metadata["Description"]
-project = metadata["Name"]
-title = metadata["Name"]
-version = metadata["Version"]
-release = metadata["Version"] + ""  #full release (includes alpha, beta, ...)
-
+add_paths()
+set_metadata()
 
 #extensions
 extensions = [
