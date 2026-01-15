e90c938d9dc60f115f8b5bdd54416d6c4354be3c
Lukas Steinwender <lukas.steinwender99@gmail.com>
2026-01-16 10:40:44 +1100

    docstrings for backends

--- docs/_templates/autosummary/base.rst
+++ docs/_templates/autosummary/base.rst
@@ -1,13 +1,16 @@
 .. base template for autosummary
 
+.. display name as main title
 {{ fullname }}
 {{ "=" * fullname|length }}
 
+.. generate toc
 .. autosummary::
     :signatures: none
     :toctree: generated/
 
     {{ fullname }}
 
+.. generate docs
 .. automodule:: {{ module }}
    :members:
---
+++ src/lstein/backends/__init__.py
@@ -0,0 +1,17 @@
+"""classes for displaying LStein in different backends
+
+- each class contains convenience methods for plotting an existing LSteinCanvas instance
+- each class returns the most fundamental parent when creating the plot
+    - allows access to all children and their attributes for customization
+
+Exceptions
+
+Classes
+    - LSteinPlotly  -- use Plotly as the backend
+    - LSteinMPL     -- use matplotlib as the backend
+
+Functions
+
+Other Objects
+
+"""
