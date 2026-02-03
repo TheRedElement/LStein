23c789a0135eadae2928ed8a06952031343ed76b
Lukas Steinwender <lukas.steinwender99@gmail.com>
2026-02-03 16:26:24 +1100

    conversion to md

--- docs/conf.py
+++ docs/conf.py
@@ -115,7 +115,7 @@ def copy_files(app):
     text = (src / f).read_text("utf-8")
     text = re.sub(r"^\>\s+", r"", text, flags=re.MULTILINE)
     text = re.sub(r"\[!(\w+)\]", r"{\1}", text)
-    text = f"```{text}\n```"
+    text = "```" + {text} + "```"
 
     (dst / f).write_text(text, encoding="utf-8")
 
